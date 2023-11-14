7161052abd1047a956dfc244b7c59516f58ac628
rodrigo azevedo <razevedocosta@gmail.com>
2023-11-14 15:25:57 -0300

    utilizando orientação a objetos

--- obj/Debug/net8.0/projeto-musical.AssemblyInfo.cs
+++ obj/Debug/net8.0/projeto-musical.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projeto-musical")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+813ac668b4978e5849c282a2b40aa1757dd3a218")]
 [assembly: System.Reflection.AssemblyProductAttribute("projeto-musical")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projeto-musical")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
