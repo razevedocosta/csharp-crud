c9db39c86212acf2f63c8a4e8784737a683b1f45
rodrigo azevedo <razevedocosta@gmail.com>
2023-11-14 16:31:05 -0300

    usando api, arquivos e linq

---
+++ Filtros/LinqOrder.cs
@@ -0,0 +1,13 @@
+using Modelos;
+
+namespace Filtros;
+
+internal class LinqOrder {
+    public static void ExibirListaDeArtistasOrdenados(List<Musica> musicas) {
+        var artistasOrdenados = musicas.OrderBy(musica => musica.Artista).Select(musica => musica.Artista).Distinct().ToList();
+        Console.WriteLine("Lista de artistas ordenados");
+        foreach (var artista in artistasOrdenados) {
+            Console.WriteLine($"- {artista}");
+        }
+    }
+}
